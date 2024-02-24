d689d7c28839621054cf62bf197722f45d1a04a8
Xedoken <kubabuba103@gmail.com>
2024-02-24 15:23:46 +0100

    cleanup code

--- VelocidroneQoLClass.cs
+++ VelocidroneQoLClass.cs
@@ -10,28 +10,18 @@ using System.Security.Cryptography.X509Certificates;
 using System.Reflection;
 using VelocidroneQoL;
 
-
-
 namespace VelocidroneQoL
 {
     public class VelocidroneQoLClass : MelonMod
     {
         //CAMERA CONTROL CameraContoller.fpvFieldOfView
-        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
-        {
-            LoggerInstance.Msg($"Scene {sceneName} with build index {buildIndex} has been loaded!");
-        }
-
-
         public override void OnUpdate()
         {
             CameraContoller Camera = GameObject.Find("Camera").GetComponent<CameraContoller>();
-            //hffkblekiic
             if (Input.GetKeyUp(KeyCode.LeftBracket))
             {
                 Camera.fpvFieldOfView += 2;
                 Camera.setFpvFOVMinus();
-
             }
         }
     }
